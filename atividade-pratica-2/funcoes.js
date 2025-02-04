ea94d250888882a3757211fa873751be664afe22
flaviare1s <flaviarster@gmail.com>
2025-02-04 08:46:26 -0300

    Atividades práticas da faculdade

---
+++ atividade-pratica-2/funcoes.js
@@ -0,0 +1,24 @@
+// 1.
+function saudacao() {
+  console.log('Olá, mundo!')
+}
+
+saudacao()
+
+// 2.
+
+function novaSaudacao (nome) {
+  console.log(`Olá, ${nome}!`)
+}
+
+novaSaudacao('João')
+
+// 3. 
+
+function soma(a, b) {
+  return a + b
+}
+
+console.log(soma(2, 3))
+console.log(soma(24, 3))
+console.log(soma(7, 47));
